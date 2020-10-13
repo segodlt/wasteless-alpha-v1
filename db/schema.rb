dc428c79e6d743155a401f4604e0e2a72ee22181
Segolene de la Taille <segolette@hotmail.com>
2020-10-13 16:31:57 +0200

    creation models recipe et category

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_134315) do
+ActiveRecord::Schema.define(version: 2020_10_13_142953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.text "alert"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "difficulty"
+    t.integer "duration"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_recipes_on_category_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "units", force: :cascade do |t|
@@ -45,4 +64,6 @@ ActiveRecord::Schema.define(version: 2020_10_13_134315) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "categories"
+  add_foreign_key "recipes", "users"
 end
